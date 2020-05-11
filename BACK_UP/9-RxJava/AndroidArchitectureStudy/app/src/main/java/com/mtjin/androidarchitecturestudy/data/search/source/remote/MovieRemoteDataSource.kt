095b45d6d7b3ebb3cc0bc0d358a9961b0bc8a89b
jackjackE <tmddjs210@naver.com>
2020-05-12 02:09:35 +0900

    remote 중간 커밋 local insert 가 데이터가 안넣어지는중

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/remote/MovieRemoteDataSource.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/remote/MovieRemoteDataSource.kt
@@ -1,12 +1,11 @@
 package com.mtjin.androidarchitecturestudy.data.search.source.remote
 
-import com.mtjin.androidarchitecturestudy.data.search.Movie
+import com.mtjin.androidarchitecturestudy.data.search.MovieResponse
+import io.reactivex.Single
 
 interface MovieRemoteDataSource {
     fun getSearchMovies(
         query: String,
-        start: Int = 1,
-        success: (List<Movie>) -> Unit,
-        fail: (Throwable) -> Unit
-    )
+        start: Int = 1
+    ): Single<MovieResponse>
 }

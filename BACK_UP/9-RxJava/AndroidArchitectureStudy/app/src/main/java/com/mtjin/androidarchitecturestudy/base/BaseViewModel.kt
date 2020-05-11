623b0648004f6904aeb1df373c2a690b3bfdf169
jackjackE <tmddjs210@naver.com>
2020-05-11 16:44:24 +0900

    BaseViewModel 생성

---
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/base/BaseViewModel.kt
@@ -0,0 +1,13 @@
+package com.mtjin.androidarchitecturestudy.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+        super.onCleared()
+    }
+}

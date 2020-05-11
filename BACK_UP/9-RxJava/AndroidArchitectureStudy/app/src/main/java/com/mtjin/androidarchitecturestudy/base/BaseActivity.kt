d619b084abc524f28789610a7ffce6684a148e43
jackjackE <tmddjs210@naver.com>
2020-05-11 16:44:08 +0900

    compositeDisposable 추가

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/base/BaseActivity.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/base/BaseActivity.kt
@@ -2,10 +2,18 @@ package com.mtjin.androidarchitecturestudy.base
 
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
 
     protected fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
 }
+

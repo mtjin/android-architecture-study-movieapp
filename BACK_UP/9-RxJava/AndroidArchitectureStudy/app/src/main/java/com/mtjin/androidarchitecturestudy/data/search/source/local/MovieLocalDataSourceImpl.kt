329a1726879cdd5492183d64137a4727b5971d92
jackjackE <tmddjs210@naver.com>
2020-05-12 02:12:27 +0900

    불필요한 코드 제거

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSourceImpl.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSourceImpl.kt
@@ -1,10 +1,8 @@
 package com.mtjin.androidarchitecturestudy.data.search.source.local
 
-import android.util.Log
 import com.mtjin.androidarchitecturestudy.data.search.Movie
 import io.reactivex.Completable
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 
 class MovieLocalDataSourceImpl(private val movieDao: MovieDao) : MovieLocalDataSource {
     override fun insertMovies(movies: List<Movie>): Completable {
@@ -22,15 +20,12 @@ class MovieLocalDataSourceImpl(private val movieDao: MovieDao) : MovieLocalDataS
 
     override fun getSearchMovies(title: String): Single<List<Movie>> {
         return movieDao.getMoviesByTitle(title)
-            .subscribeOn(Schedulers.io())
             .map {
-                Log.d("AAAA", "getALLMOVIES()   " + it.size )
                 it
             }
     }
 
     override fun deleteAllMovies(): Completable {
         return movieDao.deleteAllMovies()
-            .subscribeOn(Schedulers.io())
     }
 }

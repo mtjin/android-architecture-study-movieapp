83aa2bbbca21b31bb3bca4db397e3948983d7c40
jackjackE <tmddjs210@naver.com>
2020-05-11 18:01:27 +0900

    LocalDataSource 수정

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSource.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSource.kt
@@ -1,10 +1,12 @@
 package com.mtjin.androidarchitecturestudy.data.search.source.local
 
 import com.mtjin.androidarchitecturestudy.data.search.Movie
+import io.reactivex.Completable
+import io.reactivex.Single
 
 interface MovieLocalDataSource {
-    fun insertMovies(movies: List<Movie>)
-    fun getAllMovies(): List<Movie>
-    fun getSearchMovies(title: String): List<Movie>
-    fun deleteAllMovies()
+    fun insertMovies(movies: List<Movie>): Completable
+    fun getAllMovies(): Single<List<Movie>>
+    fun getSearchMovies(title: String): Single<List<Movie>>
+    fun deleteAllMovies(): Completable
 }
--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSourceImpl.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieLocalDataSourceImpl.kt
@@ -1,21 +1,36 @@
 package com.mtjin.androidarchitecturestudy.data.search.source.local
 
 import com.mtjin.androidarchitecturestudy.data.search.Movie
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 class MovieLocalDataSourceImpl(private val movieDao: MovieDao) : MovieLocalDataSource {
-    override fun insertMovies(movies: List<Movie>) {
-        movieDao.insertMovies(movies)
+    override fun insertMovies(movies: List<Movie>): Completable {
+        return movieDao
+            .insertMovies(movies)
+            .subscribeOn(Schedulers.io())
     }
 
-    override fun getAllMovies(): List<Movie> {
-        return movieDao.getAllMovies()
+    override fun getAllMovies(): Single<List<Movie>> {
+        return movieDao
+            .getAllMovies()
+            .subscribeOn(Schedulers.io())
+            .map {
+                it
+            }
     }
 
-    override fun getSearchMovies(title: String): List<Movie> {
+    override fun getSearchMovies(title: String): Single<List<Movie>> {
         return movieDao.getMoviesByTitle(title)
+            .subscribeOn(Schedulers.io())
+            .map {
+                it
+            }
     }
 
-    override fun deleteAllMovies() {
-        movieDao.deleteAllMovies()
+    override fun deleteAllMovies(): Completable {
+        return movieDao.deleteAllMovies()
+            .subscribeOn(Schedulers.io())
     }
 }

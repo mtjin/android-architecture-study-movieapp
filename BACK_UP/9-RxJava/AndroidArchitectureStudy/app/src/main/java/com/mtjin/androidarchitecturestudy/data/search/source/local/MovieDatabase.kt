f3fa4785271d3afe48ae20c67585b4788648f663
jackjackE <tmddjs210@naver.com>
2020-05-11 18:02:08 +0900

    코드정리

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieDatabase.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/local/MovieDatabase.kt
@@ -1,8 +1,6 @@
 package com.mtjin.androidarchitecturestudy.data.search.source.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mtjin.androidarchitecturestudy.data.search.Movie
 

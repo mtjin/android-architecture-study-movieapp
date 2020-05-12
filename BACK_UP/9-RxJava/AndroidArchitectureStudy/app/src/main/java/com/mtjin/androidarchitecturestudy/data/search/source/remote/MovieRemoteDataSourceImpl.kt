da9b6ccf918cfea201af87025eb5569210d8ff06
jackjackE <tmddjs210@naver.com>
2020-05-12 12:32:50 +0900

    코드정리

--- BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/remote/MovieRemoteDataSourceImpl.kt
+++ BACK_UP/9-RxJava/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/search/source/remote/MovieRemoteDataSourceImpl.kt
@@ -12,38 +12,5 @@ class MovieRemoteDataSourceImpl(private val apiInterface: ApiInterface) :
             .map {
                 it
             }
-
-
-//        val movieCall = apiInterface.getSearchMovie(query, start)
-//        movieCall.enqueue(object : Callback<MovieResponse> {
-//            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-//                Log.d(TAG, "Remote onFailure")
-//                fail(t)
-//            }
-//
-//            override fun onResponse(
-//                call: Call<MovieResponse>,
-//                response: Response<MovieResponse>
-//            ) {
-//                with(response) {
-//                    val body = body()
-//                    if (isSuccessful && body != null) {
-//                        Log.d(TAG, "Remote onResponse success")
-//                        if (start != body.start) {
-//                            fail(Throwable("마지막 페이지"))
-//                        } else {
-//                            success(body.movies)
-//                        }
-//                    } else {
-//                        Log.d(TAG, "Remote onResponse fail")
-//                        fail(HttpException(this))
-//                    }
-//                }
-//            }
-//        })
-    }
-
-    companion object {
-        const val TAG = "MovieRemoteDataSource"
     }
 }
